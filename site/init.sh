7fe63e586efe14c379df3a5abee26892358066c5
Javier Carrascal <javilumbrales@gmail.com>
2018-07-31 12:33:12 +0200

    Init settings script

---
+++ site/init.sh
@@ -0,0 +1,27 @@
+ wp plugin enable akismet
+ wp plugin enable autoptimize
+ wp plugin enable host-analyticsjs-local
+ wp plugin enable contact-form-7
+ wp plugin enable cookie-law-info
+ wp plugin enable wp-smushit
+ wp plugin enable wp-gallery-custom-links
+ wp plugin enable wordpress-seo
+ wp plugin enable updraftplus
+ #wp plugin enable gallery-lightbox-slider
+
+
+ wp option update permalink_structure "/%category%/%postname%/"
+ wp option update timezone_string "Europe/Madrid"
+
+ wp option update wordpress_api_key "870adb89f494"
+
+ wp option update autoptimize_js_exclude seal.js
+ wp option update autoptimize_html on
+ wp option update autoptimize_js on
+ wp option update autoptimize_js_include_inline on
+ wp option update autoptimize_css on
+
+
+ wp option update caos_allow_tracking cookie_is_set
+ wp option update sgal_cookie_notice_name viewed_cookie_policy
+
